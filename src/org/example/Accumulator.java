b4c360694f6df1556d8610f24086d02b694839cd
alvaropallares20 <alvaropallaresm@gmail.com>
2022-09-21 12:38:12 +0200

    Programa de incrementar

---
+++ src/org/example/Accumulator.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Accumulator {
+
+    private int value;
+
+    public Accumulator(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void increment(){
+        value = value + 1;
+    }
+}
