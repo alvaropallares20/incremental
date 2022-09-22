99fb2e01187e9618bf28f0bec99b74f723985440
alvaropallares20 <alvaropallaresm@gmail.com>
2022-09-22 12:24:56 +0200

    incremetal

--- src/org/example/Main.java
+++ src/org/example/Main.java
@@ -1,18 +1,26 @@
 package org.example;
 
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
+
        Accumulator acomulator = new Accumulator(0);
+       LinkedList<Thread> listaHilos = new LinkedList<>();
 
         for (int i = 0; i < 100; i++) {
             Thread thread = new Thread(new IncrementTask(acomulator, 1000));
             thread.start();
+            listaHilos.add(thread);
         }
-        try{
-            Thread.sleep(15000);
-        }catch(InterruptedException e){
-            throw new RuntimeException(e);
-        }
+
+            for (Thread incrementThread:listaHilos) {
+                try{
+                    incrementThread.join();
+                }catch(InterruptedException e){
+                    throw new RuntimeException(e);
+                }
+            }
 
         System.out.println(acomulator.getValue());
     }
